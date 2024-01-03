d9d295e176c733a62a6aaa18ef02674f2d04c4ef
aalexeee22 <alex2aalexeee@gmail.com>
2024-01-03 18:11:15 +0200

    prima_mea_componenta

---
+++ firma_constructii/src/app/pagina-principala/pagina-principala.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-principala',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-principala.component.html',
+  styleUrl: './pagina-principala.component.scss'
+})
+export class PaginaPrincipalaComponent {
+
+}
