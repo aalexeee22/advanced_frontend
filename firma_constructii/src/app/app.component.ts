0a4f44a1f71074fabb9f3432b39a38ba0ad3b857
aalexeee22 <alex2aalexeee@gmail.com>
2024-01-05 11:33:46 +0200

    . Am folosit prima componenta creata

--- firma_constructii/src/app/app.component.ts
+++ firma_constructii/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import {PaginaPrincipalaComponent } from './pagina-principala/pagina-principala.component';//am importat componenta
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,PaginaPrincipalaComponent],//aici se adauga componenta importata
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
